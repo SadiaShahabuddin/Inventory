e0fa8b0c0e8a6770feecaf2ec90295a342a91212
Ridwan <yt.rahat@gmail.com>
2023-10-15 19:55:45 +0600

    bug fixed

--- Inventory/wwwroot/js/vendors.js
+++ Inventory/wwwroot/js/vendors.js
@@ -4,7 +4,67 @@ $(document).ready(function () {
     loadDataTable();
 });
 
+//your Code
+//function loadDataTable() {
+//    dataTable = $('#tblData').DataTable({
+//        "ajax": {
+//            "url": "/Vendors/GetAll",
+//            "type": "GET",
+//            "datatype": "json"
+//        },
+//        "columns": [
+//            { "data": "vendorName", "width": "5%" },
+//            { "data": "vendorTypeId", "width": "5%" },
+//            { "data": "address", "width": "5%" },
+//            { "data": "city", "width": "5%" },
+//            { "data": "state", "width": "5%" },
+//            { "data": "zipCode", "width": "5%" },
+//            { "data": "phone", "width": "5%" },
+//            { "data": "email", "width": "5%" },
+//            { "data": "contactPerson", "width": "5%" },
+//            {
+//                "data": "id",
+//                "render": function (data) {
 
+//                    return `<div class="text-center">
+//                            <a href="/Vendors/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
+//                                <i class='far fa-edit'></i> Edit
+//                            </a>
+//                            &nbsp;
+//                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/Vendors/Delete/'+${data})>
+//                               <i class='far fa-trash-alt'></i> Delete
+//                            </a></div>
+//                        `;
+//                }, "width": "30%"
+//            }
+
+
+//        ],
+//        "language": {
+//            "emptyTable": "no data found."
+//        },
+//        "width": "100%"
+//    });
+//}
+//function loadDataTable() {
+//    $.ajax({
+//        url: "/Vendors/GetAll",
+//        type: "GET",
+//        dataType: "json",
+//        success: function (data) {
+//            // Log the response data to the console
+//            console.log(data);
+
+//            // You can process the data here or perform any other actions as needed
+//        },
+//        error: function (xhr, status, error) {
+//            // Handle any errors here
+//            console.error("Error: " + error);
+//        }
+//    });
+//}
+
+//Ridwan Code
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
@@ -21,17 +81,16 @@ function loadDataTable() {
             { "data": "zipCode", "width": "5%" },
             { "data": "phone", "width": "5%" },
             { "data": "email", "width": "5%" },
-            { "data": "contactPerson", "width": "5%" },
             {
                 "data": "id",
                 "render": function (data) {
 
                     return `<div class="text-center"> 
-                            <a href="/Vendors/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
+                            <a href="/ProductTypes/Upsert/${data}" class='btn btn-success text-white' style='cursor:pointer; width:100px;' >
                                 <i class='far fa-edit'></i> Edit
                             </a>
                             &nbsp;
-                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/Vendors/Delete/'+${data})>
+                            <a class='btn btn-danger text-white' style='cursor:pointer; width:100px;' onclick=Delete('/ProductTypes/Delete/'+${data})>
                                <i class='far fa-trash-alt'></i> Delete
                             </a></div>
                         `;
@@ -47,7 +106,8 @@ function loadDataTable() {
     });
 }
 
-function Delete(url) {
+function 
+Delete(url) {
     swal({
         title: "Are you sure want to Delete?",
         text: "You will not be able to restore the file!",
