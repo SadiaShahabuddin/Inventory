777051bb8d89a3406bf2a49916cb7abc75dae2af
Sadia Athoi <sadiashahabuddinathoi@gmail.com>
2023-10-19 13:02:10 +0600

    Customer Type drop-down add in Customers/Upsert.cshtml

--- Inventory/Models/Branch.cs
+++ Inventory/Models/Branch.cs
@@ -1,27 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Inventory.Models
 {
     public class Branch
     {
 
-            [Key]
-            public int Id { get; set; }
-            [Required]
-            public string BranchName { get; set; }
-            public string Description { get; set; }
-            [Display(Name = "Currency")]
-            public int CurrencyId { get; set; }
-            [Display(Name = "Street Address")]
-            public string Address { get; set; }
-            public string City { get; set; }
-            public string State { get; set; }
-            [Display(Name = "Zip Code")]
-            public string ZipCode { get; set; }
-            public string Phone { get; set; }
-            public string Email { get; set; }
-            [Display(Name = "Contact Person")]
-            public string ContactPerson { get; set; }
-        }
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string BranchName { get; set; }
+        public string Description { get; set; }
+        [Display(Name = "Currency")]
+        public int CurrencyId { get; set; }
+        [Display(Name = "Street Address")]
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        [Display(Name = "Contact Person")]
+        public string ContactPerson { get; set; }
+        [NotMapped]
+        public string CurrencyName { get; set; }
     }
+}
 
