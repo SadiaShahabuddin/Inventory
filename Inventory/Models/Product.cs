4d0386807d4696b6bd3d9d3467149c1ab07a32cf
Ridwan <yt.rahat@gmail.com>
2023-10-23 14:21:39 +0600

    add product

--- Inventory/Models/Product.cs
+++ Inventory/Models/Product.cs
@@ -8,26 +8,36 @@ namespace Inventory.Models
         [Key]
         public int Id { get; set; }
         public string ProductName { get; set; }
+        public string ProductCode { get; set; }
+        public string Barcode { get; set; }
         public string Description { get; set; }
         public Byte[] Image { get; set; }
-        public int Price { get; set; }
+        public string ProductImageUrl { get; set; }
 
+        [Display(Name = "UOM")]
+        public int UnitOfMeasureId { get; set; }
+        public double DefaultBuyingDefaultSellingPrice { get; set; } = 0.0;
+        [Display(Name ="Default Selling DefaultSellingPrice")]
+        public double DefaultSellingDefaultSellingPrice { get; set; } = 0.0;
+
+        [Display(Name = "Branch")]
+        public int BranchId { get; set; }
+        [Display(Name = "Currency")]
+        public int CurrencyId { get; set; }
         [Required]
+
         [Display(Name = "Sub Category")]
         public int SubCategoryId { get; set; }
-
         [Required]
         [Display(Name = "Brand")]
         public int BrandId { get; set; }
         [ForeignKey("SubCategoryId")]
         public virtual SubCategory SubCategory { get; set; }
 
-
         [ForeignKey("BrandId")]
         public virtual Brand Brand { get; set; }
 
         [NotMapped]
         public string CategoryName { get; set; }
-
     }
 }
