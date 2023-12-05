4ba2632cbbb39b3e6932f7b05d058fd51daf0b76
Ridwan <yt.rahat@gmail.com>
2023-12-05 10:53:26 +0600

    bangladesh

--- Inventory/Controllers/InvoiceTypesController.cs
+++ Inventory/Controllers/InvoiceTypesController.cs
@@ -7,10 +7,12 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Inventory.Data;
 using Inventory.Models;
-
+using Microsoft.AspNetCore.Authorization;
 
 namespace Inventory.Controllers
 {
+    [Authorize]
+
     public class InvoiceTypesController : Controller
     {
         private readonly ApplicationDbContext _context;
